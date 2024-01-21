ca14a870399ab58b4ba72eb52da9092fa5deb4e8
dillimaharjan <maharjan@pythian.com>
2024-01-21 20:10:14 +0545

    Adding version

--- puppy.go
+++ puppy.go
@@ -1,8 +1,13 @@
 package puppy
 
+import "fmt"
+
 func Bark() string {
 	return "Wolf"
 }
 func Barks() string {
 	return "Wolf Wolf Wolf"
+}
+func from12() {
+	fmt.Println("This is from version 1.2")
 }
